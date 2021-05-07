c43af02a478750c6945bb13a44ab4567af8a05ae
Robert Witczyk <rwitczyk@wp.pl>
2021-05-07 23:34:51 +0200

    Dodany navbar i mdbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { MdbModule } from 'mdb-angular-ui-kit';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        MdbModule,
+        BrowserAnimationsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
